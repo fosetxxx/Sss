8000b7103d644100022fae5e6e533ec238553b00
Semih Karahan <karahansemih@hotmail.com>
2023-05-24 22:17:35 +0300

    Initial Commit

---
+++ CustomerArrays/CustomerArraysApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomerArraysApp.swift
+//  CustomerArrays
+//
+//  Created by Semih Karahan on 24.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomerArraysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
